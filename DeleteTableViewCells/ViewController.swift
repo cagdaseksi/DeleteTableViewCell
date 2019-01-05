8e492a1c8786af598eea7582efb9aa55eff59b4f
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-05 22:25:05 +0300

    Initial Commit

---
+++ DeleteTableViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeleteTableViewCells
+//
+//  Created by MAC on 5/1/19.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
